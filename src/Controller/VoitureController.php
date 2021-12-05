2c25d83bc1b1981fa55888b5fbc611c9c54f5093
Amir <Amir.yazidi1@esprit.tn>
2021-12-06 00:55:04 +0100

    Pagination

--- src/Controller/VoitureController.php
+++ src/Controller/VoitureController.php
@@ -3,18 +3,24 @@
 namespace App\Controller;
 
 use App\Repository\VoitureRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class VoitureController extends AbstractController
 {
     /**
      * @Route("/client/voitures", name="voitures")
      */
-    public function index(VoitureRepository $repo): Response
+    public function index(VoitureRepository $repo, PaginatorInterface $paginatorInterface, Request $request): Response
     {
-        $voitures=$repo->findAll();
+        $voitures= $paginatorInterface->paginate(
+            $repo->findAllWithPagination(),
+            $request->query->getInt('page', 1), /*page number*/
+            6 /*limit per page*/
+        );
         return $this->render('voiture/voitures.html.twig',[
             "voitures" =>$voitures
         ]);
